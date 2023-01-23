54441e4e1ae0c049f80d9a56aed06de5f66b0c9c
coder3001 <122210584+coder3001@users.noreply.github.com>
2023-01-23 13:11:38 +0200

    accessing_items

--- data_structure.py
+++ data_structure.py
@@ -1,3 +1,5 @@
+"LISTS"
+
 letters = ["a", "b", "c"]
 matrix = [[0, 1], [2, 3]]
 zeros = [0] * 5
@@ -12,3 +14,20 @@ numbers = list(range(20))
 chars = list("Hello World")
 print(chars)
 print(len(chars))
+
+
+"ACCESSING_ITEMS"
+
+letters = ["a", "b", "c", "d"]
+letters[0] = "A"
+print(letters[0])
+print(letters[-1])
+print(letters[0:3])
+print(letters[:3])
+print(letters[0:])
+print(letters[:])
+print(letters[::2])
+
+numbers = list(range(20))
+print(numbers[::2])
+print(numbers[::-1])
