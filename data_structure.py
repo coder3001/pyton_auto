52759e47e4236407af03365cec925b1296e718d8
coder3001 <122210584+coder3001@users.noreply.github.com>
2023-01-23 11:24:57 +0200

    Create data_structure.py

---
+++ data_structure.py
@@ -0,0 +1,14 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+print(zeros)
+combined = zeros + letters
+print(combined)
+
+numbers = list(range(20))
+print(numbers)
+
+numbers = list(range(20))
+chars = list("Hello World")
+print(chars)
+print(len(chars))
